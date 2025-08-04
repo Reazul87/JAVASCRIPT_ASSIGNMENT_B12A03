035b237973f2b74a5257c697e2b25a1c857eff80
Reazul87 <hasanreazul87@gmail.com>
2025-08-04 14:38:42 +0700

    1st_task.js

---
+++ 1st_task.js
@@ -0,0 +1,9 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 800;
+//write your code here
+
+var equalDistribution = area / 2 ;
+
+if(equalDistribution){
+    console.log("Each brother will get :",equalDistribution) ;
+}
