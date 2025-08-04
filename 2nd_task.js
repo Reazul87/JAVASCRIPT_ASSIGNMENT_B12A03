a5e7c1aaef7b233f1207884ba6adfebd45e98232
Reazul87 <hasanreazul87@gmail.com>
2025-08-04 14:39:08 +0700

    2nd_task.js

---
+++ 2nd_task.js
@@ -0,0 +1,15 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+
+if(money >= 25000){
+console.log(" will get Laptop ") ;
+}
+
+else if(money >= 10000){
+console.log(" will get Cycle ") ;
+}
+
+else{
+console.log(" will get Chocolate ") ;
+}
